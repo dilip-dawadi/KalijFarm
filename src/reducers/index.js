68c0f11097d9a80874ed740ab7020548481a17b7
dilip-zan <85009048+dilip-zan@users.noreply.github.com>
2022-01-25 22:57:12 +0545

    first commit

--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers} from 'redux';
-import Kalijs from './Kalijs'
-import Auth from './Auth';
-import About from './About';
-export const reducers = combineReducers({ Kalijs, Auth, About });
